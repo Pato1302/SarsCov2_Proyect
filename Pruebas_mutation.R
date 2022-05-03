058b31c8a8bce53821ca210f6561898bc5733ab4
PameRuiz <a01423595@tec.mx>
2022-05-03 12:48:13 -0500

    Updated function to work with a vector of countries

--- Pruebas_mutation.R
+++ Pruebas_mutation.R
@@ -5,8 +5,17 @@ cat("\f")
 library(seqinr)
 
 original = read.fasta("Archivos/Wuhan_coding_sequences.txt")
-mexican = read.fasta("Archivos/first_B_sequences/first_B_USA.txt")
 
+df = data.frame(
+  pais = character(),
+  gen = character(),
+  mutation = character(),
+  nucleo = numeric(),
+  codon = character(),
+  protein = character(),
+  index = numeric(),
+  protein_change = logical()
+)
 
 abreviatura = function(codon){
   return(switch(codon,"GAC"="D","GAU"="D",
@@ -35,49 +44,48 @@ adn_to_arnm = function(elemento){
   return (switch(elemento,"C"="C","G"="G","A"="A","T"="U"))
 }
 
-Mutaciones = function(original, otra, g){
-  df = data.frame(
-    pais = character(),
-    gen = character(),
-    mutation = character(),
-    nucleo = numeric(),
-    codon = character(),
-    protein = character(),
-    index = numeric(),
-    protein_change = logical()
-  )
-  
-  genWuhan = original[[g]]
-  genWuhan = toupper(genWuhan)
-  genWuhan = as.vector(sapply(genWuhan,adn_to_arnm))
-  
-  fila = 1
-  
-  for(k in seq(g,length(mexican),12)){ #g+12 por que en la espícula del primer registro hay una inserción y el df tiene más de 2000 filas
-    genMexico = mexican[[k]];      
-    genMexico = toupper(genMexico)
-    genMexico = as.vector(sapply(genMexico,adn_to_arnm))
-    print(length(genMexico))
-    print(length(genWuhan))
-    for(i in seq(1, min(c(length(genMexico), length(genWuhan))), 1)){
-      if(genWuhan[i] != genMexico[i]){
-        codonIndex = as.integer((i) %/% 3) + 1 
-        codonWuhan = paste(c(genWuhan[((codonIndex*3)-2):(codonIndex*3)]), collapse = "")
-        codonMexico = paste(c(genMexico[((codonIndex*3)-2):(codonIndex*3)]), collapse = "")
-        df[fila, 3] = paste(c(genWuhan[i],genMexico[i]),collapse = " to ")
-        df[fila, 4] = i + length(mexican[[k-2]]) + length(mexican[[k-1]]) #Al descargar el archivo con m?ltiples secuencias, no contiene el atributo del ?ndice de inicio del gen S
-        #Entonces el ?ndice que da es la suma de la longitud de los 2 genes previos y el de la regi?n codificante del gen S.
-        df[fila, 5] = paste(c(codonWuhan,codonMexico), collapse = " to ") 
-        df[fila, 6] = paste(c(abreviatura(codonWuhan), abreviatura(codonMexico)), collapse = " to ")
-        df[fila, 7] = codonIndex
-        fila = fila + 1
-      }
+Mutaciones = function(original, vector_paises){
+  for (p in seq (1,length(vector_paises),1)) {
+    mexican = read.fasta(paste(c("Archivos/first_B_sequences/first_B_",vector_paises[p],".txt"),collapse=""))
+    genWuhan = original[[3]]
+    genWuhan = toupper(genWuhan)
+    genWuhan = as.vector(sapply(genWuhan,adn_to_arnm))
+    
+    if (length(mexican)==12) {
+      g = 3
+    } else {
+      g = 2
     }
-    df[fila, ] = ""   #Dejamos un espacio entre cada genoma para facilitar la visualización
-    fila = fila + 1
+    
+    fila = 1
+    
+    for(k in seq(g,length(mexican),12)){ #g+12 por que en la espícula del primer registro hay una inserción y el df tiene más de 2000 filas
+      genMexico = mexican[[k]];      
+      genMexico = toupper(genMexico)
+      genMexico = as.vector(sapply(genMexico,adn_to_arnm))
+      for(i in seq(1, min(c(length(genMexico), length(genWuhan))), 1)){
+        if(genWuhan[i] != genMexico[i]){
+          codonIndex = as.integer((i) %/% 3) + 1 
+          codonWuhan = paste(c(genWuhan[((codonIndex*3)-2):(codonIndex*3)]), collapse = "")
+          codonMexico = paste(c(genMexico[((codonIndex*3)-2):(codonIndex*3)]), collapse = "")
+          df[fila, 1] = vector_paises[p]
+          df[fila, 3] = paste(c(genWuhan[i],genMexico[i]),collapse = " to ")
+          df[fila, 4] = i + length(mexican[[k-2]]) + length(mexican[[k-1]]) #Al descargar el archivo con m?ltiples secuencias, no contiene el atributo del ?ndice de inicio del gen S
+          #Entonces el ?ndice que da es la suma de la longitud de los 2 genes previos y el de la regi?n codificante del gen S.
+          df[fila, 5] = paste(c(codonWuhan,codonMexico), collapse = " to ") 
+          df[fila, 6] = paste(c(abreviatura(codonWuhan), abreviatura(codonMexico)), collapse = " to ")
+          df[fila, 7] = codonIndex
+          fila = fila + 1
+        }
+      }
+      df[fila, ] = ""   #Dejamos un espacio entre cada genoma para facilitar la visualización
+      fila = fila + 1
+    }
   }
   return(df)
 }
 
-dataFrame_genS = Mutaciones(original,mexican,3)
+vector_paises = c("Francia","Tailandia","Japon","Italia","USA","Mexico")
+
+dataFrame_genS = Mutaciones(original,vector_paises)
 print(dataFrame_genS)
